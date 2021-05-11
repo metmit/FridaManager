eb55d0bdef4a9e0d9e9f3836f28bd7034ea85c04
ruesin <ruesin@gmail.com>
2021-05-11 21:43:51 +0800

    home

--- app/src/main/java/com/metmit/frida/manager/ui/home/HomeFragment.java
+++ app/src/main/java/com/metmit/frida/manager/ui/home/HomeFragment.java
@@ -1,35 +1,101 @@
 package com.metmit.frida.manager.ui.home;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.metmit.frida.manager.R;
+import com.metmit.frida.manager.utils.Frida;
+import com.metmit.frida.manager.utils.ShellHelper;
+import com.metmit.frida.manager.utils.SpHelper;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private View rootView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        init();
+
+        final Switch fridaSwitch = rootView.findViewById(R.id.switchFridaStatus);
+
+        fridaSwitch.setChecked(Frida.isRunning());
+
+        fridaSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (fridaSwitch.isChecked()) {
+                    Frida.startService();
+                } else {
+                    Frida.stopService();
+                }
             }
         });
-        return root;
+
+        return rootView;
+    }
+
+
+    protected void init() {
+
+        // Frida 版本
+        TextView textViewFridaVersion = rootView.findViewById(R.id.textViewFridaVersion);
+        String homeFridaVersionString = getString(R.string.home_frida_version);
+        String fridaVersion = Frida.getVersion();
+        if (TextUtils.isEmpty(fridaVersion)) {
+            homeFridaVersionString = String.format(homeFridaVersionString, "无法获取");
+        } else {
+            homeFridaVersionString = String.format(homeFridaVersionString, fridaVersion);
+        }
+        textViewFridaVersion.setText(homeFridaVersionString);
+
+        // root 权限
+        TextView textViewRoot = rootView.findViewById(R.id.textViewRoot);
+        String homeRootString = getString(R.string.home_root_permission);
+        if (new ShellHelper().executeSu("ps").getCode() == ShellHelper.SUCCESS_CODE) {
+            homeRootString = String.format(homeRootString, "已获得");
+        } else {
+            homeRootString = String.format(homeRootString, "未获得");
+        }
+        textViewRoot.setText(homeRootString);
+
+        SpHelper spHelper = new SpHelper(getContext(), SpHelper.SP_NAME_SETTINGS);
+
+        // 开机启动
+        TextView textViewBoot = rootView.findViewById(R.id.textViewBoot);
+        String homeBootString = getString(R.string.home_boot);
+        if (spHelper.getSp().getBoolean(SpHelper.SP_KEY_BOOT_FRIDA, false)) {
+            homeBootString = String.format(homeBootString, "是");
+        } else {
+            homeBootString = String.format(homeBootString, "否");
+        }
+        textViewBoot.setText(homeBootString);
+
+        // 设备型号
+        TextView textViewDevice = rootView.findViewById(R.id.textViewDeviceType);
+        String homeDeviceString = getString(R.string.home_device_type);
+        homeDeviceString = String.format(homeDeviceString, Build.MANUFACTURER + " " + Build.BRAND + " " + Build.MODEL + " ("+ Build.DEVICE + ")");
+        textViewDevice.setText(homeDeviceString);
+
+        // 系统版本
+        TextView textViewSystem = rootView.findViewById(R.id.textViewSystemVersion);
+        String homeSystemString = getString(R.string.home_system_version);
+        textViewSystem.setText(String.format(homeBootString, String.format(" Android %s (API %s)", Build.VERSION.RELEASE, Build.VERSION.SDK_INT)));
+
+        // 系统架构
+        TextView textViewAbi= rootView.findViewById(R.id.textViewAbi);
+        String homeAbiString = getString(R.string.home_abi);
+        textViewAbi.setText(String.format(homeAbiString, Build.SUPPORTED_ABIS[0]));
     }
 }
--- app/src/main/java/com/metmit/frida/manager/utils/Frida.java
+++ app/src/main/java/com/metmit/frida/manager/utils/Frida.java
@@ -1,6 +1,14 @@
 package com.metmit.frida.manager.utils;
 
+import android.text.TextUtils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 public class Frida {
+
+    public static int DEFAULT_PORT = 27042;
 
     protected static String servicePath = "/data/local/tmp/";
 
@@ -28,5 +36,63 @@ public class Frida {
         }
 
         return true;
+    }
+
+    public static ArrayList<String> getProcessIds() {
+        ShellHelper shellHelper = new ShellHelper().executeSu("pidof frida-server");
+        if (shellHelper.getCode() != 0) {
+            return new ArrayList<>();
+        }
+        String res = shellHelper.getResult();
+        String[] temp = res.split("\n");
+
+        ArrayList<String> result = new ArrayList<>();
+        for (String pid : temp) {
+            String tmp = pid.replace(" ", "").trim();
+            if (TextUtils.isEmpty(tmp)) continue;
+            result.add(tmp);
+        }
+        return result;
+    }
+
+    public static boolean stopService() {
+        if (!isExistCommandFile()) {
+            return false;
+        }
+        ArrayList<String> process = getProcessIds();
+        ShellHelper shellHelper = new ShellHelper();
+        for (String pid : process) {
+            shellHelper.executeSu("kill -9 " + pid);
+        }
+        return true;
+    }
+
+    public static String getVersion() {
+        if (!isExistCommandFile()) {
+            return null;
+        }
+
+        ShellHelper shellHelper = new ShellHelper();
+        shellHelper.executeSu("chmod +x " + servicePath + commandFile);
+
+        ShellHelper result = shellHelper.executeSu(servicePath + commandFile + " --version");
+        if (result.getCode() != 0) {
+            return null;
+        }
+
+        return result.getResult();
+    }
+
+    public static boolean isRunning() {
+        if (getProcessIds().size() > 0) {
+            return true;
+        }
+        try {
+            new ServerSocket(DEFAULT_PORT).close();
+        } catch (IOException e) {
+            Helper.log(e.getMessage());
+            return true;
+        }
+        return false;
     }
 }
